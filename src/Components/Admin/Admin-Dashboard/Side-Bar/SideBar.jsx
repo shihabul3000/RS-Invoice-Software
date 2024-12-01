1aa52ea3e9fb2a343339fbca73d54e53a08835e8
unknown <shihabul72@gmail.com>
2024-12-01 14:49:08 +0600

    side bar problem fixing v-2

--- src/Components/Admin/Admin-Dashboard/Side-Bar/SideBar.jsx
+++ src/Components/Admin/Admin-Dashboard/Side-Bar/SideBar.jsx
@@ -1,16 +1,25 @@
 import './SideBar.css';
-import { FaTachometerAlt, FaChartLine, FaFileInvoice, FaCalendarAlt, FaRegCalendar, FaComments, FaBell, FaCog } from 'react-icons/fa';  // Import the icons you need
+import {
+  FaTachometerAlt,
+  FaChartLine,
+  FaFileInvoice,
+  FaCalendarAlt,
+  FaRegCalendar,
+  FaComments,
+  FaBell,
+  FaCog,
+} from 'react-icons/fa';
 
 const SideBar = () => {
   const menuItems = [
-    { name: 'Dashboard', icon: <FaTachometerAlt /> },
-    { name: 'Analytics', icon: <FaChartLine /> },
-    { name: 'Invoice', icon: <FaFileInvoice /> },
-    { name: 'Schedule', icon: <FaCalendarAlt /> },
-    { name: 'Calendar', icon: <FaRegCalendar /> },
-    { name: 'Messages', icon: <FaComments /> },
-    { name: 'Notification', icon: <FaBell /> },
-    { name: 'Settings', icon: <FaCog /> },
+    { name: 'Dashboard', icon: <FaTachometerAlt />, notifications: false },
+    { name: 'Analytics', icon: <FaChartLine />, notifications: false },
+    { name: 'Invoice', icon: <FaFileInvoice />, notifications: false },
+    { name: 'Schedule', icon: <FaCalendarAlt />, notifications: false },
+    { name: 'Calendar', icon: <FaRegCalendar />, notifications: false },
+    { name: 'Messages', icon: <FaComments />, notifications: true, },
+    { name: 'Notification', icon: <FaBell />, notifications: false },
+    { name: 'Settings', icon: <FaCog />, notifications: false },
   ];
 
   return (
@@ -21,26 +30,34 @@ const SideBar = () => {
           alt="Logo"
           className="logoImage"
         />
-        <h1 className="logoText">Side Bar</h1>
+        <h1 className="logoText">Wons</h1>
       </div>
       <div className="menu">
         {menuItems.map((item, index) => (
           <div key={index} className="menuItem">
             <span className="icon">{item.icon}</span>
             <span className="menuText">{item.name}</span>
+            {item.notifications && (
+              <span className="notificationBadge">{item.count}</span>
+            )}
           </div>
         ))}
+      </div>
+      <div className="upgrade">
+        <div className="upgradeContent">
+          {/* <span>Upgrade Now</span> */}
+        </div>
       </div>
       <div className="logout">
         <img
           src="/public/Images/My-Picture-f.jpg"
+          alt="Profile"
           className="profileImage"
         />
         <div className="logoutText">
           <span className="name">Shihabul Islam Alvi</span>
-          <span className="accountType">Font End Developer - React</span>
+          <span className="accountType">Front End Developer - React</span>
         </div>
-        
       </div>
     </div>
   );
